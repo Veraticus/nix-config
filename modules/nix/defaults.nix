17b66e76c8eec8bfbb4056e9883f3b5fa4aa2b44
Josh Symonds <josh@joshsymonds.com>
2025-12-04 18:35:31 -0800

    Fix mkdefault

--- modules/nix/defaults.nix
+++ modules/nix/defaults.nix
@@ -24,13 +24,15 @@
         "https://nix-community.cachix.org"
         "https://neovim-nightly.cachix.org"
         "https://joshsymonds.cachix.org"
+        "https://devenv.cachix.org"
       ];
       extra-trusted-public-keys = lib.mkDefault [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "neovim-nightly.cachix.org-1:fLrV5fy41LFKwyLAxJ0H13o6FOVGc4k6gXB5Y1dqtWw="
         "joshsymonds.cachix.org-1:DajO7Bjk/Q8eQVZQZC/AWOzdUst2TGp8fHS/B1pua2c="
+        "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
       ];
-      trusted-users = lib.mkDefault ["root" "joshsymonds"];
+      trusted-users = ["root" "joshsymonds"];
     };
   };
 }

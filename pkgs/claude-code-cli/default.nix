fad9d7a659f2a9739d1f98ae631554350d0b1987
Josh Symonds <josh@joshsymonds.com>
2026-01-18 10:17:57 -0800

    cc update

--- pkgs/claude-code-cli/default.nix
+++ pkgs/claude-code-cli/default.nix
@@ -7,11 +7,11 @@
 }:
 stdenv.mkDerivation rec {
   pname = "claude-code-cli";
-  version = "2.1.11";
+  version = "2.1.12";
 
   src = fetchurl {
     url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
-    hash = "sha256-WWhmjUp71FRcuT9lfubx9pVMKl/BAbWF9DxCdzfgCmM=";
+    hash = "sha256-ltKE++NvgGBrT9XfliKXyc+NeewdmnSqjRbfk7t/BoU=";
   };
 
   nativeBuildInputs = [makeWrapper];

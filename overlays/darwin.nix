19b7338515d42e877b3efa512b4ed792d55f0edc
Josh Symonds <josh@joshsymonds.com>
2026-02-18 16:05:07 -0800

    Some darwin cleanups and CC upgrade

--- overlays/darwin.nix
+++ overlays/darwin.nix
@@ -3,10 +3,6 @@ final: prev: let
 in
   if isDarwin
   then {
-    gtk3 = prev.gtk3.overrideAttrs (old: {
-      patches = (old.patches or []) ++ [../patches/gtk3-darwin-sincos.patch];
-    });
-
     aerospace = final.callPackage ../pkgs/aerospace {};
   }
   else {}
--- pkgs/claude-code-cli/default.nix
+++ pkgs/claude-code-cli/default.nix
@@ -6,25 +6,25 @@
   glibc,
 }:
 let
-  version = "2.1.45";
+  version = "2.1.47";
   gcsBase = "https://storage.googleapis.com/claude-code-dist-86c565f3-f756-42ad-8dfa-d59b1c096819/claude-code-releases/${version}";
 
   sources = {
     "aarch64-darwin" = fetchurl {
       url = "${gcsBase}/darwin-arm64/claude";
-      hash = "sha256-5lrxa5tzLOAdZCPCgffmolW1u4hQeAvs+ZkVM4bjf9Q=";
+      hash = "sha256-c3lebY8KpOB9jyDTUeD4TlFdss5ztpdwZQvD1dWC73M=";
     };
     "x86_64-darwin" = fetchurl {
       url = "${gcsBase}/darwin-x64/claude";
-      hash = "sha256-0Q0C5rza58yNUaCDy36TM+eS027XPJSWA175VbOUtrk=";
+      hash = "sha256-tiiJsNTR8Hv3br6JttGxuFaQl3qTkwe8iWzXZ0jfMDE=";
     };
     "x86_64-linux" = fetchurl {
       url = "${gcsBase}/linux-x64/claude";
-      hash = "sha256-5+hHODxGZplHB3Qejlar+E3SQwFTYg07pf9ahJN15N4=";
+      hash = "sha256-nEi95nvaJ01lw9ZdpPeOIaRYznIqiVXtzCctMsmMdKM=";
     };
     "aarch64-linux" = fetchurl {
       url = "${gcsBase}/linux-arm64/claude";
-      hash = "sha256-5njw6BEuRwqScjrWiD7LGUDRQRVI9njyHqTpzEEFfn8=";
+      hash = "sha256-klXTMNsZNT1zs5dbC8Lrrd0c8AKmL6Fblaa7/sipvhg=";
     };
   };
 in
